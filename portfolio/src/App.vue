d18655ef47e694336dfdf690f7cb393db12e14f0
Micka070702 <mickael.aitelli.7@gmail.com>
2023-11-30 16:25:33 +0100

    ADD: debut du projet / test de la techno

---
+++ portfolio/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/project">Project</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2e2e2e;
+}
+
+nav a.router-link-exact-active {
+  color: #2272FF;
+}
+
+body {
+    margin: 0 auto;
+    background-color: #111827;
+}
+
+</style>
---
+++ portfolio/src/views/ProjectView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="project">
+      <img
+        alt="Vue logo"
+        src="../assets/kc.jpeg"
+        style="width: 100%; height: auto;"
+      />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+  </template>
+
+<script>
+    // @ is an alias to /src
+    import HelloWorld from '@/components/ProjectPage.vue'
+
+    export default {
+        name: 'ProjectView',
+        components: {
+            HelloWorld
+        }
+    }
+</script>
