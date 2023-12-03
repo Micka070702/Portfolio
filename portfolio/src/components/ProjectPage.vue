2480620786cdf26967487c9f21620dcfb1ee4c27
Micka070702 <mickael.aitelli.7@gmail.com>
2023-12-04 00:23:03 +0100

    [Add]: type de projet [Fix]: responsive des images (format Pc et pas telephone)

--- portfolio/src/components/ProjectPage.vue
+++ portfolio/src/components/ProjectPage.vue
@@ -14,11 +14,12 @@
                     </div>
                     <div class="project-text">
                         <h3>{{ project.title }}</h3>
-                        <p>{{ project.description }}</p>
+                        <p v-html="project.description"></p><br>
 
                         <div class="project-tech">
-                            <p>{{ project.tech }}</p>
+                            <span v-for="(tech, index) in project.tech" :key="index">{{ tech }}{{ index < project.tech.length - 1 ? '   ' : '' }}</span>
                         </div>
+                        <img class="project-type" :src="require('@/assets/' + project.type)">
                     </div>
                 </div>
             </div>
@@ -37,23 +38,26 @@
                     {
                         id: 1,
                         title: 'My paint',
-                        description: 'Recréation de Paint en équipe de deux.Travail effectué en environ trois semaines. Fonctionnalités clés incluant tracé de lignes, remplissage, et sélection de couleurs.',
-                        tech: 'Vue.js',
-                        image: 'miniaMyPaint.png'
+                        description: 'Recréation de Paint en équipe de deux.<br>Travail effectué en environ trois semaines.<br><br>Fonctionnalités clés incluant: tracé de lignes, remplissage, et sélection de couleurs.',
+                        tech: ['C', 'CSFML'],
+                        image: 'miniaMyPaint.png',
+                        type: 'epitech.png'
                     },
                     {
                         id: 2,
-                        title: 'Project test 1',
-                        description: 'Recréation de Paint en équipe de deux.Travail effectué en environ trois semaines. Fonctionnalités clés incluant tracé de lignes, remplissage, et sélection de couleurs.',
-                        tech: 'Vue.js',
-                        image: 'miniaMyPaint.png'
+                        title: 'My RPG',
+                        description: 'Création d\'un RPG en équipe de 4.<br>Travail effectué en environ quatres semaines.<br><br>Fonctionnalités clés incluant: Caractéristiques, Combats, et sélection de couleurs.',
+                        tech: ['C', 'CSFML'],
+                        image: 'miniaMyRPG.png',
+                        type: 'epitech.png'
                     },
                     {
                         id: 3,
-                        title: 'Project test 1',
-                        description: 'Recréation de Paint en équipe de deux.Travail effectué en environ trois semaines. Fonctionnalités clés incluant tracé de lignes, remplissage, et sélection de couleurs.',
-                        tech: 'Vue.js',
-                        image: 'miniaMyPaint.png'
+                        title: 'My RPG',
+                        description: 'Création d\'un RPG en équipe de 4.<br>Travail effectué en environ quatres semaines.<br><br>Fonctionnalités clés incluant: Caractéristiques, Combats, et sélection de couleurs.',
+                        tech: ['C', 'CSFML'],
+                        image: 'miniaMyRPG.png',
+                        type: 'epitech.png'
                     }
                 ]
             };
@@ -64,7 +68,7 @@
 
 <style scoped>
     .all-page {
-        margin-left: 10%;
+        margin-left: 15%;
     }
     .main-title {
         text-align: left;
@@ -74,7 +78,7 @@
         flex-wrap: wrap;
     }
     .project {
-        width: 44%;
+        width: 40%;
         margin-bottom: 0.8%;
         margin-right: 0.8%;
         height: 600px; /* Hauteur fixe des projet */
@@ -84,22 +88,33 @@
         flex-direction: column;
         border: 1px solid #20283b;
         border-radius: 9px;
-        height: 100%; /* Remplir la hauteur du projet */
+        height: 100%;
         background-color: #000a28;
     }
     .img-container {
         overflow: hidden;
-        height: 60%; /* Ajuster la hauteur de l'image */
+        width: 100%;
+        height: auto;
     }
     .img {
         width: 100%;
-        height: 100%;
+        height: auto;
         border-top-left-radius: 9px;
         border-top-right-radius: 9px;
     }
     .project-text {
         text-align: left;
         margin-left: 2%;
-        height: 40%; /* Ajuster la hauteur du texte */
+        height: 40%;
+    }
+    .project-tech span {
+        margin-right: 15px;
+    }
+    .project-type {
+        width: 30px;
+        display: flex;
+        position:relative;
+        bottom: 5%;
+        right: -90%;
     }
   </style>
